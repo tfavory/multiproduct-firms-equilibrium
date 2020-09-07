ebd46a485668cb64d9167c7c01d2dd57813c8f39
tfavory <43109272+tfavory@users.noreply.github.com>
2020-09-07 19:30:40 +0800

    Update main.py
    More flexibility to create a Market object with more firms

--- main.py
+++ main.py
@@ -3,10 +3,14 @@
 from . import Market
 from scipy.stats import uniform, norm, expon, gumbel_r
 
+number_firms = 3
+products_per_firms = [1] * number_firms
+marginal_costs = [0] * number_firms
+distributions = [gumbel_r()] * number_firms
 
-market = Market(products_per_firms=[1,1,1],
-                marginal_costs=[0,0,0],
-                distributions=[uniform(), uniform(), uniform()],
+market = Market(products_per_firms=products_per_firms,
+                marginal_costs=marginal_costs,
+                distributions=distributions,
                 outside_option=False)
 
 
